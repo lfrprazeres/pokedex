0fd4743930a4e1d4bdc810461a6a25cafa0b89ad
Luiz Fernando <lfrprazeres@gmail.com>
2020-07-13 09:27:01 -0300

    Updates

---
+++ precache-manifest.ccf2eaa5c3094552bf3c660de61d1ea0.js
@@ -0,0 +1,294 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8076bc0236f01920b2472c820b65746c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97b9052f8d9b121ff423",
+    "url": "/static/css/main.4b320bcf.chunk.css"
+  },
+  {
+    "revision": "939874b03cbde786874d",
+    "url": "/static/js/2.52198334.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.52198334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97b9052f8d9b121ff423",
+    "url": "/static/js/main.799e4960.chunk.js"
+  },
+  {
+    "revision": "250103716dfc89e5abb8",
+    "url": "/static/js/runtime-main.9ff9079d.js"
+  },
+  {
+    "revision": "1318f9dbc3e1c3c9dcf6b364812fb906",
+    "url": "/static/media/Filter.1318f9db.svg"
+  },
+  {
+    "revision": "cfde1a89be9beb6c62deb24e670533a4",
+    "url": "/static/media/Generation.cfde1a89.svg"
+  },
+  {
+    "revision": "fb3ad68919dcae09e97e21197a6d71d2",
+    "url": "/static/media/I.fb3ad689.svg"
+  },
+  {
+    "revision": "ce4bc3b8f1dfe393ab735f50826a859a",
+    "url": "/static/media/II.ce4bc3b8.svg"
+  },
+  {
+    "revision": "645a25620d8f67b77dfaf4106e75da01",
+    "url": "/static/media/III.645a2562.svg"
+  },
+  {
+    "revision": "dadfd4f5ff9f836448d1d2d1090ca15b",
+    "url": "/static/media/IV.dadfd4f5.svg"
+  },
+  {
+    "revision": "ed9bc7542a908d8ee98ceb6f3adfa06b",
+    "url": "/static/media/Pattern.ed9bc754.svg"
+  },
+  {
+    "revision": "8748e3182047caffb9c9d944bd84096e",
+    "url": "/static/media/Pokeball.8748e318.svg"
+  },
+  {
+    "revision": "d09549c1ab4a5947a007561521e45da3",
+    "url": "/static/media/SFProDisplay-Regular.d09549c1.ttf"
+  },
+  {
+    "revision": "d041c767381d572e422af041e4eacf8a",
+    "url": "/static/media/Search.d041c767.svg"
+  },
+  {
+    "revision": "3be09818711c42adbba1b5b4a709e104",
+    "url": "/static/media/Sort.3be09818.svg"
+  },
+  {
+    "revision": "c5a17f69ca00744ad1766e41c4ab25b9",
+    "url": "/static/media/V.c5a17f69.svg"
+  },
+  {
+    "revision": "2a0f80e6ae1e7699363ed7fcc11aa92f",
+    "url": "/static/media/VI.2a0f80e6.svg"
+  },
+  {
+    "revision": "0e1c2017dfb91c2f022219621bebd2e6",
+    "url": "/static/media/VII.0e1c2017.svg"
+  },
+  {
+    "revision": "ec26f5f006c77edd2728ce58d20327c7",
+    "url": "/static/media/VIII.ec26f5f0.svg"
+  },
+  {
+    "revision": "becef2f5f8587b0e9b044d8c13bd7270",
+    "url": "/static/media/back.becef2f5.svg"
+  },
+  {
+    "revision": "4656a47c5350db7264841b43bf7001e7",
+    "url": "/static/media/bug.4656a47c.svg"
+  },
+  {
+    "revision": "9b2a13a7b72a122f1ebb0f1b531c4909",
+    "url": "/static/media/bugUnselected.9b2a13a7.svg"
+  },
+  {
+    "revision": "56c03741a169cbf68358d7df161b17aa",
+    "url": "/static/media/dark.56c03741.svg"
+  },
+  {
+    "revision": "8af3d5cb6dc8b83f2cd7af259d324d27",
+    "url": "/static/media/darkUnselected.8af3d5cb.svg"
+  },
+  {
+    "revision": "5853781a0dc7ed4d806ec18c1a08d34d",
+    "url": "/static/media/dragon.5853781a.svg"
+  },
+  {
+    "revision": "b7161eb8fbd851ff7dd7e731b3ad6624",
+    "url": "/static/media/dragonUnselected.b7161eb8.svg"
+  },
+  {
+    "revision": "d104a74ec81681b36feaa10511c41391",
+    "url": "/static/media/electric.d104a74e.svg"
+  },
+  {
+    "revision": "bc07194772f86e58aba4047b7fee814a",
+    "url": "/static/media/electricUnselected.bc071947.svg"
+  },
+  {
+    "revision": "b8bdda449667a3efa050701841ae9467",
+    "url": "/static/media/fairy.b8bdda44.svg"
+  },
+  {
+    "revision": "33f930654d1b07ded4d24bc06d2c97ad",
+    "url": "/static/media/fairyUnselected.33f93065.svg"
+  },
+  {
+    "revision": "4b925afe39daa972514eba4a2f6e87b3",
+    "url": "/static/media/fighting.4b925afe.svg"
+  },
+  {
+    "revision": "a96f04efe58ebea46416aba9a365b6f5",
+    "url": "/static/media/fightingUnselected.a96f04ef.svg"
+  },
+  {
+    "revision": "b98343b6169938b61fa98ed28a408600",
+    "url": "/static/media/fire.b98343b6.svg"
+  },
+  {
+    "revision": "4b8f244a10f94e18beb1470246cb2fc8",
+    "url": "/static/media/fireUnselected.4b8f244a.svg"
+  },
+  {
+    "revision": "28df97c50e82887df9d75ad8310a5172",
+    "url": "/static/media/flying.28df97c5.svg"
+  },
+  {
+    "revision": "9e465edae678f4c8ca0eb506446e454c",
+    "url": "/static/media/flyingUnselected.9e465eda.svg"
+  },
+  {
+    "revision": "ccb1758c5f774a1d4079605ffeb578ad",
+    "url": "/static/media/ghost.ccb1758c.svg"
+  },
+  {
+    "revision": "36f988d22e8c26bfd27c5cad499a006b",
+    "url": "/static/media/ghostUnselected.36f988d2.svg"
+  },
+  {
+    "revision": "ae3a4da977a1f14dcd6e27482b2f3abb",
+    "url": "/static/media/grass.ae3a4da9.svg"
+  },
+  {
+    "revision": "faea92d5608c0a0b0855bed0c007d19d",
+    "url": "/static/media/grassUnselected.faea92d5.svg"
+  },
+  {
+    "revision": "07bdb2ad247108dcc2c409ba95bb70b5",
+    "url": "/static/media/ground.07bdb2ad.svg"
+  },
+  {
+    "revision": "690b81045a0f6c66921ea25365d1d9a9",
+    "url": "/static/media/groundUnselected.690b8104.svg"
+  },
+  {
+    "revision": "03800e12648a85b4594413df47f7f288",
+    "url": "/static/media/half.03800e12.svg"
+  },
+  {
+    "revision": "9f8c1d10f32a10fd6558cf7835630261",
+    "url": "/static/media/heavy.9f8c1d10.svg"
+  },
+  {
+    "revision": "62da7e346293fe7f13c5682aaa4a59da",
+    "url": "/static/media/heavyUnselected.62da7e34.svg"
+  },
+  {
+    "revision": "137fc791ddb283c667698316c4420d92",
+    "url": "/static/media/ice.137fc791.svg"
+  },
+  {
+    "revision": "fb963b105925f43b801d180b7c2efc4b",
+    "url": "/static/media/iceUnselected.fb963b10.svg"
+  },
+  {
+    "revision": "ffdb93b2ef68243df09aa2eb36a76ab9",
+    "url": "/static/media/light.ffdb93b2.svg"
+  },
+  {
+    "revision": "7fad9bccb2e81b454f93603cae9a2336",
+    "url": "/static/media/lightUnselected.7fad9bcc.svg"
+  },
+  {
+    "revision": "64b400deb457e6e343c8707ffaf31178",
+    "url": "/static/media/medium.64b400de.svg"
+  },
+  {
+    "revision": "928d82ccacc8fcf949d23223bbc0dc22",
+    "url": "/static/media/mediumUnselected.928d82cc.svg"
+  },
+  {
+    "revision": "1bacb721c9f37b8ee9c50463d603c50d",
+    "url": "/static/media/normal.1bacb721.svg"
+  },
+  {
+    "revision": "a259d5ff9f352634fd8866658ebadf1e",
+    "url": "/static/media/normal.a259d5ff.svg"
+  },
+  {
+    "revision": "04b61a7044b7cb831402113ee679b6d2",
+    "url": "/static/media/normalUnselected.04b61a70.svg"
+  },
+  {
+    "revision": "2737eda24216cd513ad3ce045ce4707f",
+    "url": "/static/media/normalUnselected.2737eda2.svg"
+  },
+  {
+    "revision": "96d0eb180f8d6658317b9aa11681c83d",
+    "url": "/static/media/poison.96d0eb18.svg"
+  },
+  {
+    "revision": "7413a6ca72a79b0749a0f09972812264",
+    "url": "/static/media/poisonUnselected.7413a6ca.svg"
+  },
+  {
+    "revision": "63e32d336c9afd6f4ac49674530ab340",
+    "url": "/static/media/pokeballBackground.63e32d33.png"
+  },
+  {
+    "revision": "f974386cc84d5e2e087c1c3067f763fb",
+    "url": "/static/media/pokeball_loading.f974386c.gif"
+  },
+  {
+    "revision": "8f18a6254bd225f8a4818f4593f77150",
+    "url": "/static/media/psychic.8f18a625.svg"
+  },
+  {
+    "revision": "0e665de6a3943d7824815fc0e03bd8b8",
+    "url": "/static/media/psychicUnselected.0e665de6.svg"
+  },
+  {
+    "revision": "7baefb2e4dcbed50c57eccb9d213ca45",
+    "url": "/static/media/rock.7baefb2e.svg"
+  },
+  {
+    "revision": "059132000cb6833428aec0797ff0996f",
+    "url": "/static/media/rockUnselected.05913200.svg"
+  },
+  {
+    "revision": "9d407b7bb11ebda2efecae3f3b79899b",
+    "url": "/static/media/short.9d407b7b.svg"
+  },
+  {
+    "revision": "55be2983635b76d698387f6246fac0ba",
+    "url": "/static/media/shortUnselected.55be2983.svg"
+  },
+  {
+    "revision": "a4ace405c2b829e149fc11029fe36e94",
+    "url": "/static/media/steel.a4ace405.svg"
+  },
+  {
+    "revision": "ee7d75a7b8224283a59c9adeabe74b4c",
+    "url": "/static/media/steelUnselected.ee7d75a7.svg"
+  },
+  {
+    "revision": "ebb9044dba3d906146520b7acd2be7f4",
+    "url": "/static/media/tall.ebb9044d.svg"
+  },
+  {
+    "revision": "4c959457b630ee38be66526576f3d5aa",
+    "url": "/static/media/tallUnselected.4c959457.svg"
+  },
+  {
+    "revision": "848fd76fa461a39985f083644a067ad0",
+    "url": "/static/media/water.848fd76f.svg"
+  },
+  {
+    "revision": "7b69c548d16d961ad37a3c8e716bcfb7",
+    "url": "/static/media/waterUnselected.7b69c548.svg"
+  }
+]);
